8179833d52589c75d8a9a443f47655d35d01484a
Nicolás Miari <nicolasmiari@gmail.com>
2022-08-14 14:36:57 +0900

    Implement custom Codable support, test (coverage 100%).

--- Sources/Entity/Entity.swift
+++ Sources/Entity/Entity.swift
@@ -1,7 +1,7 @@
 import Foundation
 import Identifier
 
-public struct EntityIdentifier: Identifier {
+public struct EntityIdentifier: Identifier, Codable {
 
   // The entity identifier is just a wrapper around UUID, but that is an implementation detail (the
   // abstraction allows us to change this in the future).
@@ -9,5 +9,22 @@ public struct EntityIdentifier: Identifier {
 
   public init() {
     contents = UUID()
+  }
+
+  // MARK: - Codable
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.singleValueContainer()
+    let value = try container.decode(String.self)
+    guard let uuid = UUID(uuidString: value) else {
+      let context = EncodingError.Context(codingPath: [], debugDescription: "Cannot create UUID from string \"\(value)\"")
+      throw EncodingError.invalidValue(value, context)
+    }
+    self.contents = uuid
+  }
+
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.singleValueContainer()
+    try container.encode(contents.uuidString)
   }
 }
--- Tests/EntityTests/EntityTests.swift
+++ Tests/EntityTests/EntityTests.swift
@@ -20,5 +20,35 @@ final class EntityTests: XCTestCase {
     // THEN
     XCTAssertNotEqual(id1, id2)
   }
+
+  func testEncodingDecodingPreservesContent() throws {
+    // GIVEN
+    let id = EntityIdentifier()
+
+    // WHEN
+    let data = try JSONEncoder().encode(id)
+    let recovered = try JSONDecoder().decode(EntityIdentifier.self, from: data)
+
+    // THEN
+    XCTAssertEqual(id, recovered)
+  }
+
+  func testDecodeWellFormedStringSucceeds() throws {
+    // GIVEN
+    let string = "\"82217999-5182-4EB4-B82E-82E2F1A67860\""
+    let data = string.data(using: .utf8)!
+
+    // THEN
+    XCTAssertNoThrow(try JSONDecoder().decode(EntityIdentifier.self, from: data))
+  }
+
+  func testDecodeMalformedStringFails() throws {
+    // GIVEN
+    let string = "\"82217999-5182-4EB4-B82E-\""
+    let data = string.data(using: .utf8)!
+
+    // THEN
+    XCTAssertThrowsError(try JSONDecoder().decode(EntityIdentifier.self, from: data))
+  }
 }
 
